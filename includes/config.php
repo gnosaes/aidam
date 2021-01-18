20ffb87b8a366ad06d4ec49c231c870f590f6f4d
juasfan <juah198@gmail.com>
2021-01-18 19:02:58 +0800

    update database config

--- includes/config.php
+++ includes/config.php
@@ -1,16 +1,12 @@
-<?php 
+<?php
 // DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','library');
+define('DB_HOST', 'remotemysql.com');
+define('DB_USER', 'pAxgCHC0g0');
+define('DB_PASS', 'TgdtzuByaK');
+define('DB_NAME', 'pAxgCHC0g0');
 // Establish database connection.
-try
-{
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+try {
+    $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+} catch (PDOException $e) {
+    exit("Error: " . $e->getMessage());
 }
-catch (PDOException $e)
-{
-exit("Error: " . $e->getMessage());
-}
-?>
