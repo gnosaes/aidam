13ac7c17730dd0d1e07bad1218750eb8e40c7e6c
Aina Najihah <ainanajihah98@gmail.com>
2021-01-20 07:32:31 +0800

    update index

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ if (isset($_POST['login'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
   <meta name="description" content="" />
   <meta name="author" content="" />
-  <title>Online Library Management System | </title>
+  <title>Online Library Management System </title>
   <!-- BOOTSTRAP CORE STYLE  -->
   <link href="assets/css/bootstrap.css" rel="stylesheet" />
   <!-- FONT AWESOME STYLE  -->
@@ -52,18 +52,23 @@ if (isset($_POST['login'])) {
   <link href="assets/css/style.css" rel="stylesheet" />
   <!-- GOOGLE FONT -->
   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+  
 
 </head>
 
+
 <body>
+
   <!------MENU SECTION START-->
-  <?php include('includes/header.php'); ?>
+<?php include('includes/header.php');  ?>
+ 
+ 
   <!-- MENU SECTION END-->
   <div class="content-wrapper">
     <div class="container">
       <div class="row pad-botm">
         <div class="col-md-12">
-          <h4 class="header-line">USER LOGIN FORM</h4>
+          <h4 class="header-line">USER LOGIN FORM |||</h4>
         </div>
       </div>
 
@@ -105,6 +110,7 @@ if (isset($_POST['login'])) {
   </div>
   <!-- CONTENT-WRAPPER SECTION END-->
   <?php include('includes/footer.php'); ?>
+  
   <!-- FOOTER SECTION END-->
   <script src="assets/js/jquery-1.10.2.js"></script>
   <!-- BOOTSTRAP SCRIPTS  -->
